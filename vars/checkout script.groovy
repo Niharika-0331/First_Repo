038c986026db5e801ac8df871420bd6bd94a5c07
Niharika-0331 <151155370+Niharika-0331@users.noreply.github.com>
2023-11-22 19:11:32 +0530

    Create checkout script.groovy

---
+++ vars/checkout script.groovy
@@ -0,0 +1,30 @@
+
+def call() {
+    def gitUrl = 'https://github.com/Niharika-0331/First_Repo'
+    def localDir = "C:\\Users\\nbobbili\\Downloads\\Checkout\\"
+    def branch = 'main'
+
+
+// Clone the repository
+    def cloneCommand = "git clone ${gitUrl}"
+    println "Executing: ${cloneCommand}"
+    def cloneProcess = cloneCommand.execute()
+    cloneProcess.waitFor()
+
+// Change directory to the cloned repository
+    def repoDir = new File(localDir)
+    println "Changing directory to: ${repoDir}"
+
+    repoDir.with {
+
+
+// Checkout the specified branch
+        def checkoutCommand = "git checkout ${branch}"
+        println "Executing: ${checkoutCommand}"
+        def checkoutProcess = checkoutCommand.execute()
+        checkoutProcess.waitFor()
+
+
+
+    }
+}
