ac29424159b6894f9e7aad509c9ea7ad9d305ce1
Niharika-0331 <151155370+Niharika-0331@users.noreply.github.com>
2023-11-23 16:57:25 +0530

    Update checkout_script.groovy

--- vars/checkout_script.groovy
+++ vars/checkout_script.groovy
@@ -2,5 +2,5 @@ def call()
 checkout([
 $class: 'GitSCM'
 branches: [[name: 'main']], 
- userRemoteConfigs: [[url: "https://github.com/Niharika-0331/First_Repo"]]
+ userRemoteConfigs: [[url: "https://github.com/Niharika-0331/First_Repo.git"]]
 ])
