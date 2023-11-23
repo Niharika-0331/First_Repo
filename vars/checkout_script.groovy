190070b06a6a9ddeed6f21b7852a577d1a2dd2fa
Niharika-0331 <151155370+Niharika-0331@users.noreply.github.com>
2023-11-23 17:04:52 +0530

    Update checkout_script.groovy

--- vars/checkout_script.groovy
+++ vars/checkout_script.groovy
@@ -1,7 +1,4 @@
-def call(){
-checkout([
-$class: 'GitSCM'
-branches: [[name: 'dev2']], 
- userRemoteConfigs: [[url: "https://github.com/hnimmagadda1/Task.git"]]
-])
+def call(name){
+ echo "HI"
 }
+
